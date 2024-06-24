3f64c381731b6c9f31b41c2f76e5683b4001494c
leanalvarez <leanalvarez.dev@gmail.com>
2024-06-24 18:42:59 -0300

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Lean on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
